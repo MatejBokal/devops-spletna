7c6e5ee1221fccccf51e047334acefdc0cd744f7
lejkosir <lej.kosir@gmail.com>
2025-11-20 13:09:14 +0100

    server.js za HTTPS

--- code/taprav-fri/frontend/server.js
+++ code/taprav-fri/frontend/server.js
@@ -1,15 +1,21 @@
-const { createServer } = require('http');
+const { createServer } = require('https');
+const { readFileSync } = require('fs');
 const next = require('next');
 
 const port = process.env.PORT || 3000;
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
+const httpsOptions = {
+  key: readFileSync('/etc/ssl/localcerts/nextjs.key'),
+  cert: readFileSync('/etc/ssl/localcerts/nextjs.crt')
+};
+
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(httpsOptions, (req, res) => {
     handle(req, res);
   }).listen(port, (err) => {
     if (err) throw err;
-    console.log(`Next.js app running on port ${port}`);
+    console.log(`Next.js HTTPS server running on https://localhost:${port}`);
   });
 });
